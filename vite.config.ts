4a365bda1da838515c4da821580fce3b9d05a327
Payal <mrunalinikulkarni1828@gmail.com>
2025-01-03 13:01:04 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './frontend', // Ensure the root is the frontend directory
-  build: {
-    rollupOptions: {
-      input: './frontend/index.html' // Ensure the input is the index.html file in the frontend directory
-    },
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': './frontend', // This assumes you want to use the frontend directory directly in imports
-    },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
